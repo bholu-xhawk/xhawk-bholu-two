414ec29f0dcd1c16f42bb0c8756805bd14600fd7
xhawk <xh-swe@xhawk.ai>
2026-08-06 13:33:07 +0000

    XHawk: Add static student endpoint

--- node_api/src/app.js
+++ node_api/src/app.js
@@ -4,8 +4,19 @@ const app = express();
 
 app.use(express.json());
 
+const student = {
+  id: "123",
+  name: "John Doe",
+  age: 20,
+  grade: "A",
+};
+
 app.get("/", (req, res) => {
   res.json({ message: "Hello, World!" });
+});
+
+app.get("/student", (_req, res) => {
+  res.json(student);
 });
 
 const usersRouter = require('./routes/users');
