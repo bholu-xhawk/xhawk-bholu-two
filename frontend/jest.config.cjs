523799f59576ee087e89894b0cd6b49740950099
xhawk <xh-swe@xhawk.ai>
2026-08-18 10:48:11 +0000

    XHawk: Add frontend test and CI tooling

---
+++ frontend/jest.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  transform: {
+    '^.+\\.[jt]sx?$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+  },
+  testMatch: ['<rootDir>/src/**/*.test.jsx'],
+}
